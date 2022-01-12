80957dca1d452ff7d7c1eabdcaf4d7eec026d4c5
Angelo Pirola <angelo9871@gmail.com>
2022-01-12 15:47:07 +0100

    Creato DBContext del progetto e relativi modelli di entità

---
+++ Shared/Models/Entities/Persona.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DemoBlazorApp.Shared.Models.Entities
+{
+    public partial class Persona
+    {
+        public int PersonaId { get; set; }
+        
+        [Required]
+        public string Cognome { get; set; }
+        
+        [Required]
+        public string Nome { get; set; }
+        
+        [Required]
+        public string Email { get; set; }
+        
+        [Required]
+        public string Telefono { get; set; }
+    }
+}
---
+++ Shared/Models/Services/Infrastructure/BlazorAppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using DemoBlazorApp.Shared.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoBlazorApp.Shared.Models.Services.Infrastructure
+{
+    public partial class BlazorAppDbContext : DbContext
+    {
+        public BlazorAppDbContext(DbContextOptions<BlazorAppDbContext> options)
+            : base(options)
+        { 
+        }
+
+        public virtual DbSet<Persona> Persone { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Persona>(entity =>
+            {
+                entity.ToTable("Persone");
+                entity.HasKey(persona => persona.PersonaId);
+            });
+        }
+    }
+}
