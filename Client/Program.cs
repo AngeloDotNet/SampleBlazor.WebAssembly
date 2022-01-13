4b4cc3e361c446c76d1de16da4348de6137bf426
Angelo Pirola <angelo9871@gmail.com>
2022-01-13 13:03:45 +0100

    Rimossi Counter e WeatherForecast dalla soluzione

--- Client/Program.cs
+++ Client/Program.cs
@@ -13,8 +13,6 @@ namespace DemoBlazorApp.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
             builder.RootComponents.Add<App>("#app");
-
-            //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
