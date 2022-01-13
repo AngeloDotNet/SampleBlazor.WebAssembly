e284a7d1ef92e3e69acde1c015d2fcffc63efa7e
Angelo Pirola <angelo9871@gmail.com>
2022-01-13 12:02:26 +0100

    Registrato servizio per la D.I.

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,11 +1,8 @@
+using DemoBlazorApp.Shared.Models.Services.Application.Persone;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace DemoBlazorApp.Client
@@ -15,9 +12,13 @@ namespace DemoBlazorApp.Client
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddTransient<IPersonaService, EfCorePersonaService>();
 
             await builder.Build().RunAsync();
         }
