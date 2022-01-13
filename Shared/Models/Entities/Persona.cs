71faafe4f21906f59d628a01a7626f255746fa7c
Angelo Pirola <angelo9871@gmail.com>
2022-01-13 22:22:27 +0100

    Aggiunte funzionalità di creazione e modifica record

--- Shared/Models/Entities/Persona.cs
+++ Shared/Models/Entities/Persona.cs
@@ -6,16 +6,16 @@ namespace DemoBlazorApp.Shared.Models.Entities
     {
         public int PersonaId { get; set; }
         
-        [Required]
+        [Required(ErrorMessage = "Il cognome è obbligatorio")]
         public string Cognome { get; set; }
-        
-        [Required]
+
+        [Required(ErrorMessage = "Il nome è obbligatorio")]
         public string Nome { get; set; }
-        
-        [Required]
+
+        [Required(ErrorMessage = "L'indirizzo email è obbligatorio")]
         public string Email { get; set; }
-        
-        [Required]
+
+        [Required(ErrorMessage = "Il telefono è obbligatorio")]
         public string Telefono { get; set; }
     }
 }
