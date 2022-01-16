a5fdf1cf3521b329ab06b91bceebfedad6074d77
Angelo Pirola <angelo9871@gmail.com>
2022-01-16 21:18:13 +0100

    Aggiunto Swagger al progetto SERVER

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace DemoBlazorApp.Server
 {
@@ -19,11 +20,23 @@ namespace DemoBlazorApp.Server
         }
 
         public IConfiguration Configuration { get; }
+        private readonly string _policyName = "CorsPolicy";
 
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
             services.AddRazorPages();
+
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy(name: _policyName, builder =>
+                {
+                    builder.AllowAnyOrigin()
+                    .AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+                });
+            });
 
             var persistence = Persistence.EfCore;
 
@@ -35,13 +48,38 @@ namespace DemoBlazorApp.Server
                         string connectionString = Configuration.GetSection("ConnectionStrings").GetValue<string>("Default");
                         optionBuilder.UseSqlite(connectionString, options =>
                         {
-                            // Abilitazione del connection resiliency (Non � supportato dal provider di Sqlite perch� non � soggetto a errori transienti)
+                            // Abilitazione del connection resiliency (Non è supportato dal provider di Sqlite perchè non è soggetto a errori transienti)
                             // Per informazioni consultare la pagina: https://docs.microsoft.com/en-us/ef/core/miscellaneous/connection-resiliency
                         });
                     });
 
                     break;
             }
+
+            services.AddSwaggerGen(config =>
+            {
+                config.SwaggerDoc("v1", new OpenApiInfo 
+                { 
+                    Title = "Demo API Blazor App", 
+                    Version = "v1",
+                    // Description = "API example that returns the current time",
+                    // TermsOfService = new Uri("https://example.com/terms"), 
+
+                    // Contact = new OpenApiContact
+                    // {
+                    //     Name = "Nominativo contatto",
+                    //     Email = "Email contatto",
+                    //     Url = new Uri("https://twitter.com/username-contatto"),
+                    // },
+
+                    // License = new OpenApiLicense
+                    // {
+                    //     Name = "Nome licenza API",
+                    //     Url = new Uri("https://example.com/license"),
+                    // }
+                });
+            });
+
             services.AddTransient<IPersonaService, EfCorePersonaService>();
 
             //Options
@@ -54,6 +92,9 @@ namespace DemoBlazorApp.Server
             {
                 app.UseDeveloperExceptionPage();
                 app.UseWebAssemblyDebugging();
+
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DemoApiEfCoreSwagger v1"));
             }
             else
             {
@@ -64,6 +105,7 @@ namespace DemoBlazorApp.Server
             app.UseStaticFiles();
 
             app.UseRouting();
+            app.UseCors(_policyName);
 
             app.UseEndpoints(endpoints =>
             {
